652e704c5b88a0cf6e6a50dd5e64b13989abc809
vincent-tarrit <vincent@tarrit.com>
2023-04-03 15:14:51 +0200

    setup board

--- resources/js/jsPlumb.js
+++ resources/js/jsPlumb.js
@@ -1,6 +1,61 @@
 import * as jsPlumbBrowserUI from "@jsplumb/browser-ui";
+import {CONNECTION, EVENT_ELEMENT_MOUSE_UP} from "@jsplumb/browser-ui";
+import {AnchorLocations} from "@jsplumb/common";
 
 
 const instance = jsPlumbBrowserUI.newInstance({
     container: document.querySelector("#builder-area"),
+    dragOptions: {
+        containment: "parent",
+        containmentPadding: 10
+    },
+    connectionsDetachable: false
+});
+
+instance.importDefaults({
+    //connector: FlowchartConnector.type,
+    overlays: [
+        {type: "Arrow", options: {location: 1}}
+    ],
+    maxConnections: 4,
+    anchor: AnchorLocations.AutoDefault,
+
+});
+
+
+const videos = document.getElementsByClassName('node');
+
+for (const video of videos) {
+    for (const position of ['Top', 'Right', 'Bottom', 'Left']) {
+        instance.addEndpoint(video, {
+            endpoint: "Dot",
+            anchor: AnchorLocations.AutoDefault,
+            source: true,
+            target: true,
+        });
+    }
+}
+
+for (const video of videos) {
+    for (const link of JSON.parse(video.dataset.links)) {
+        instance.connect({
+            source: video,
+            target: document.getElementById(`video-${link.id}`),
+            overlays: [
+                {type: "Arrow", options: {location: 1}}
+            ],
+
+        });
+    }
+}
+
+
+instance.bind(CONNECTION, (params) => {
+    Livewire.emit('addLink', params.source.dataset.videoId, params.target.dataset.videoId);
+});
+
+instance.bind(EVENT_ELEMENT_MOUSE_UP, (element) => {
+    const position = instance.getOffset(element);
+    const id = element.dataset.videoId;
+    Livewire.emit('moveCard', id, position.x, position.y);
 });
--- resources/views/livewire/board.blade.php
+++ resources/views/livewire/board.blade.php
@@ -3,4 +3,22 @@
     <button onclick="addLink()">Add Link</button>
 </div>
 
-<div id="builder-area"></div>
+<div id="builder-area" style="width: 100%; height: 90%; background-color: orange">
+
+    @foreach($videos as $video)
+        <div
+            id="video-{{ $video->id }}"
+            data-video-id="{{ $video->id }}"
+            data-links="{{ json_encode($video->adjacents->map(fn($adjacent) => [
+                 "id"=> $adjacent->id,
+                 "content" =>$adjacent->pivot->content,
+             ])) }}"
+            class="node video-{{ $video->id }} flex bg-indigo-50 justify-center w-24 h-24 items-center absolute"
+            style="left: {{ $video->position_x }}px; top: {{ $video->position_y }}px;">
+            {{ $video->name  }}
+        </div>
+    @endforeach
+
+</div>
+
+
