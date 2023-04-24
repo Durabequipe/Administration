8cf8910f234103fdcb465f67a1945f6eee953c6b
vincent-tarrit <vincent@tarrit.com>
2023-04-25 00:15:40 +0200

    fix:size of board

--- resources/views/livewire/board.blade.php
+++ resources/views/livewire/board.blade.php
@@ -1,4 +1,4 @@
-<div id="builder-area" class="absolute w-screen h-screen z-10">
+<div id="builder-area" class="absolute w-[15000px] h-[15000px] z-10">
 
     @foreach($project->videos as $video)
         <div
